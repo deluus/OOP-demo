5f0a287c8bbd33ef6c2e498839dc1e5ffc6b2c9c
deluus polycarp <pdeluus@gmail.com>
2022-01-20 14:04:00 -0800

    updated. added js files, and content. working of fs files

---
+++ _test_/Employee.test.js
@@ -0,0 +1,43 @@
+const { describe } = require('yargs');
+const Employee = require('../lib/employee');
+
+describe ("Employee", () => {
+
+    // method
+    describe ("Initialization / constructor", () => {
+
+        it("should build with a prop x when given y", ()  => {
+
+        })
+    });
+
+    describe("getName", () => {
+        
+        // arrange
+        // create a new employee ovv
+        
+        // Act
+        // call the getName and store the results
+        getName()
+
+        // Assert 
+        // the result is equal to an expected value
+    });
+
+    describe("getId", ()  => {
+
+
+
+    });
+
+    describe("", () => {
+
+
+
+    });
+
+    describe("", () => {
+
+
+    })
+})
---
+++ index.js
@@ -0,0 +1,93 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require ('./lib/manager');
+
+const employee = [];
+
+// actions needed
+
+// ask them for manager info
+
+function askForManagerInfor (){
+
+  // prompt the user for the data
+  inquirer
+  .prompt()
+  .then((answers ) => {
+    // then  creat and store an  opbject for the manager
+    employee.push(new Manager( *pass in the data from inquirer*) );
+
+    // then "ask what they would like to do next "
+    asForNextAction ();
+
+
+  })
+
+}
+async function askForManagerInfor() {
+
+  // promt the user for data
+  const answer = await inquirer.prompt( questions)
+
+  // then create and store and boject for the manager
+  emmployee.push(new Manager(*data from inquirer*));
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What was your motivation?',
+      name: 'motivation',
+    },
+    {
+      type: 'input',
+      message: 'why did you build this project?',
+      name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'what problems did it solve?',
+        name: 'solve',
+      },
+      {
+      type: 'input',
+      message: 'what did you learn?',
+      name: 'learn'
+    },
+    {
+        type: 'input',
+        message: 'what are the necessary dependencies you need to install?',
+        name: 'install',
+        choices : [ "inquirer", "badmath"]
+      },
+      {
+        type: 'input',
+        message: 'GitHub username',
+        name: 'username'
+      },
+      {
+        type: 'input',
+        message: 'Email address',
+        name: 'address'
+      },
+  ])
+
+  .then((ansers) => {
+    const htmlTemplet =
+  
+
+
+
+  fs.writeFile( 
+    'index.html', readMeTemplate,(answers),
+ (err) => {
+ err ? console.error(err) : console.log('Generating README...');
+ } )
+
+  });
