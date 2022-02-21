8ff74b41f759fd63afec4c8e9fa031eed9ff482b
deluus polycarp <pdeluus@gmail.com>
2022-02-21 14:50:53 -0800

    updateing index.js. added functions, ask, await and asyncs. corrected get rolses

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+const render = require('./lib/template');
 const Employee = require ('./lib/Employee')
 const Manager = require ('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require ('./lib/Intern');
+const { resolve } = require('path');
+const employees = [];
 
 // const employee = [];
 
 // actions needed - all seperate fxn
 
 // ask them for manager info
+async function start() {
+
 
 function askForManagerInfo() {
 
   // prompt the user for the data
-  inquirer
-  .prompt([
+  inquirer.prompt([
   
       {
         type: 'input',
@@ -30,7 +35,7 @@ function askForManagerInfo() {
       {
         type: 'input',
         message: 'What is your managers email address?',
-        name: 'getRole',
+        name: 'getEmail',
       },
       {
           type: 'input',
@@ -38,41 +43,45 @@ function askForManagerInfo() {
           name: 'getofficeNumber',
         },
     ])
-  
-  
-  
-
-
   .then((answers) => {
     // then  create and store an  object for the manager
-    Employee.push(new manager( answers.getName, answers.getId, answers.getEmail, answers.getofficeNumber) ) 
+    employees.push(new Manager(answers.getName, answers.getId, answers.getEmail, answers.getofficeNumber)) 
+    askForNextAction();
   });
 }
-askForManagerInfo();
 
   // then "ask what they would like to do next "
 
-    askForNextAction ()
-    inquirer
+  function askForNextAction () {
+    return inquirer
     .prompt([
       {  
         type: 'list',
         name : 'next',
         message: 'What would you like to do next?',
-        choices: ['add a manager', 'add an engineer', 'add an intern', 'complete team']
+        choices: ['add an engineer', 'add an intern', 'complete team']
     
-      },
+      }
     ])
+    .then(res => {
+      switch(res.next) {
+        case 'add an engineer':
+          askForEngineerInfo();
+          break;
+        case 'add an intern':
+          askForInternInfo();
+          break;
+        default:
+          buildHtml()
+      }
+    })
+  }
   
-  
-
-
 function askForEngineerInfo() {
 
   // prompt the user for the data
-  inquirer
+  return inquirer
   .prompt([
-  
       {
         type: 'input',
         message: 'What is your engineers name?',
@@ -86,7 +95,7 @@ function askForEngineerInfo() {
       {
         type: 'input',
         message: 'What is your engineers email address?',
-        name: 'getRole',
+        name: 'getEmail',
       },
       {
           type: 'input',
@@ -94,17 +103,60 @@ function askForEngineerInfo() {
           name: 'getGithub',
         },
     ])
-  
-  
-  
-
-
   .then((answers) => {
     // then  create and store an  object for the manager
-    employee.push(new engineer( answers.getName, answers.getId, answers.getEmail, answers.getGithub) ) 
+    employees.push(new Engineer(answers.getName, answers.getId, answers.getEmail, answers.getGithub) ) 
+    console.log(employees)
+    askForNextAction();
   });
 }
-askForEngineerInfo();
+
+function askForInternInfo() {
+
+  // prompt the user for the data
+  return inquirer
+  .prompt([
+      {
+        type: 'input',
+        message: 'What is your interns name?',
+        name: 'getName',
+      },
+      {
+        type: 'input',
+        message: 'What is your interns ID?',
+        name: 'getId',
+      },
+      {
+        type: 'input',
+        message: 'What is your interns email address?',
+        name: 'getEmail',
+      },
+      {
+          type: 'input',
+          message: 'What school does your intern go to?',
+          name: 'getSchool',
+        },
+    ])
+  .then((answers) => {
+    // then  create and store an  object for the manager
+    employees.push(new Intern( answers.getName, answers.getId, answers.getEmail, answers.getSchool) ) 
+    console.log(employees)
+    askForNextAction();
+  });
+}
+
+function buildHtml() {
+  if(!fs.existsSync(path.resolve(__dirname), 'output')) {
+    fs.mkdirSync(path.resolve(__dirname), 'outut')
+  }
+  fs.writeFileSync(path.join(path.resolve(__dirname, 'output'), 'team.html'), render(employees), 'utf-8')
+}
+
+await askForManagerInfo();
+};
+
+
+start();
 
 
   
